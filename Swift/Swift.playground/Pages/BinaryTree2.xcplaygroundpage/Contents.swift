5f31a9017f93ce5bec03aa879cef44bb68c98b42
douking <wyk8916@gmai.com>
2019-12-15 09:46:34 +0800

    new page

---
+++ Swift/Swift.playground/Pages/BinaryTree2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,309 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class BinaryTreeNode {
+  var value: Int
+  var leftChild: BinaryTreeNode?
+  var rightChild: BinaryTreeNode?
+  init(_ value: Int) {
+    self.value = value
+  }
+}
+
+// 创建二叉树
+func createTree(values: [Int?]) -> BinaryTreeNode? {
+  var queue: [BinaryTreeNode?] = []
+  var root: BinaryTreeNode? = nil
+  var curr: BinaryTreeNode? = nil
+  var front: Int = 0, real: Int = -1
+  for value in values {
+    curr = nil
+    if value != nil {
+      curr = BinaryTreeNode(value!)
+    }
+    real = real + 1
+    queue.append(curr)
+    if real == 0 {
+      root = curr
+    } else {
+      if curr != nil && queue[front] != nil {
+        if real % 2 == 1 {
+          queue[front]?.leftChild = curr
+        } else {
+          queue[front]?.rightChild = curr
+        }
+      }
+      if real % 2 == 0 {
+        front = front + 1
+      }
+    }
+  }
+  return root
+}
+
+//最大深度
+func maxDepth(root: BinaryTreeNode?) -> Int {
+  guard let root = root else { return 0 }
+  return 1 + max(maxDepth(root: root.leftChild), maxDepth(root: root.rightChild))
+}
+
+//: 遍历
+//层级遍历
+func levelOrderTraverse(_ root: BinaryTreeNode?) {
+  var result = [BinaryTreeNode]()
+  var level = [BinaryTreeNode]()
+  if root != nil {
+    level.append(root!)
+  }
+  while level.count != 0 {
+    var nextLevel = [BinaryTreeNode]()
+    for node in level {
+      result.append(node)
+      if let lefeNode = node.leftChild {
+        nextLevel.append(lefeNode)
+      }
+      if let rightNode = node.rightChild {
+        nextLevel.append(rightNode)
+      }
+    }
+    level = nextLevel
+  }
+  let ans = result.map {
+    $0.value
+  }
+  
+  print(ans)
+}
+
+//先序遍历
+func preorderTraversal(_ root: BinaryTreeNode?) {
+  if root == nil {
+    return
+  }
+  print(root!.value)
+  preorderTraversal(root!.leftChild)
+  preorderTraversal(root!.rightChild)
+}
+
+func nonRecursivePreorderTraversal(_ root: BinaryTreeNode?) {
+  var result: [Int] = []
+  var stack: [BinaryTreeNode] = []
+  var node: BinaryTreeNode? = root
+  while node != nil || !stack.isEmpty {
+    if node != nil {
+      result.append(node!.value)
+      stack.append(node!)
+      node = node!.leftChild
+    } else {
+      node = stack.removeLast()
+      node = node!.rightChild
+    }
+  }
+  
+  print(result)
+}
+
+//中序遍历
+func inorderTraversal(_ root: BinaryTreeNode?) {
+  if root == nil {
+    return
+  }
+  inorderTraversal(root!.leftChild)
+  print(root!.value)
+  inorderTraversal(root!.rightChild)
+}
+
+func nonRecursiveInorderTraversal(_ root: BinaryTreeNode?) -> [BinaryTreeNode] {
+  var result: [BinaryTreeNode] = []
+  var stack: [BinaryTreeNode] = []
+  var node: BinaryTreeNode? = root
+  while node != nil || !stack.isEmpty {
+    if node != nil {
+      stack.append(node!)
+      node = node!.leftChild
+    } else {
+      node = stack.removeLast()
+      result.append(node!)
+      node = node!.rightChild
+    }
+  }
+  
+  print(result.map({ (node) -> Int in
+    node.value
+  }))
+  return result
+}
+
+//后序遍历
+func postorderTraversal(_ root: BinaryTreeNode?) {
+  if root == nil {
+    return
+  }
+  postorderTraversal(root!.leftChild)
+  postorderTraversal(root!.rightChild)
+  print(root!.value)
+}
+
+func nonRecureivePostorderTraversal(_ root: BinaryTreeNode?) {
+  class Node {
+    let elem: BinaryTreeNode?
+    var n: Int
+    init(elem: BinaryTreeNode?, n: Int = 0) {
+      self.elem = elem
+      self.n = n
+    }
+  }
+  var result: [BinaryTreeNode] = []
+  var stack: [Node] = []
+  var node: Node = Node(elem: root, n: 0)
+  while node.elem != nil || !stack.isEmpty {
+    if node.elem != nil {
+      node.n = node.n + 1
+      stack.append(node)
+      node = Node(elem: node.elem!.leftChild, n: 0)
+    } else {
+      node = stack.last!
+      node.n = node.n + 1
+      if node.n == 3 {
+        node = stack.removeLast()
+        result.append(node.elem!)
+        node = Node(elem: nil, n: 0)
+      } else {
+        node = Node(elem: node.elem!.rightChild, n: 0)
+      }
+    }
+  }
+  let ans = result.map {
+    $0.value
+  }
+  print(ans)
+}
+
+//翻转二叉树
+func reversalBinaryTree(_ root: BinaryTreeNode?) {
+  guard let root = root else { return }
+  let tempLeftChild = root.leftChild
+  root.leftChild = root.rightChild
+  root.rightChild = tempLeftChild
+  
+  reversalBinaryTree(root.leftChild)
+  reversalBinaryTree(root.rightChild)
+}
+
+let t = createTree(values: [1, 2, 3, 4, 5, nil, 6, nil, nil, nil, 7, nil, nil, 8])
+print("层级遍历:")
+levelOrderTraverse(t)
+print("先序遍历:")
+preorderTraversal(t)
+nonRecursivePreorderTraversal(t)
+print("中序遍历:")
+inorderTraversal(t)
+nonRecursiveInorderTraversal(t)
+print("后序遍历")
+postorderTraversal(t)
+nonRecureivePostorderTraversal(t)
+
+print("==========  翻转后  ==========")
+reversalBinaryTree(t)
+print("层级遍历:")
+levelOrderTraverse(t)
+print("先序遍历:")
+preorderTraversal(t)
+print("中序遍历:")
+inorderTraversal(t)
+print("后序遍历")
+postorderTraversal(t)
+
+
+//: BST
+
+// 是否是二叉查找树
+func isValidBST(root: BinaryTreeNode?) -> Bool {
+  func _helper(node: BinaryTreeNode?, _ min: Int?, _ max: Int?) -> Bool {
+    guard let node = node else { return true }
+    // 所有右子节点必须大于根节点
+    if let min = min, node.value <= min {
+      return false
+    }
+    // 所有左子节点必须小于根节点
+    if let max = max, node.value >= max {
+      return false
+    }
+    
+    return _helper(node: node.leftChild, min, node.value)
+      && _helper(node: node.rightChild, node.value, max)
+  }
+  return _helper(node: root, nil, nil)
+}
+
+//插入值
+func insertBST(root: BinaryTreeNode?, value: Int) -> BinaryTreeNode? {
+  guard let root = root else {
+    return nil
+  }
+  if value > root.value {
+    if let right = root.rightChild {
+      insertBST(root: right, value: value)
+    } else {
+      root.rightChild = BinaryTreeNode(value)
+    }
+  } else {
+    if let left = root.leftChild {
+      insertBST(root: left, value: value)
+    } else {
+      root.leftChild = BinaryTreeNode(value)
+    }
+  }
+  return root
+}
+
+//删除节点
+func deleteBST(root: BinaryTreeNode?, value: Int) -> BinaryTreeNode? {
+  guard let root = root else { return nil }
+  
+  return root
+}
+
+let values = [5, 2, 8, 1, 4, 6, 9]
+let root = createTree(values: values)
+insertBST(root: root, value: 7)
+let r = nonRecursiveInorderTraversal(root).map { (node) -> Int in
+  node.value
+}
+r
+
+print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+
+func buildTree(inorder: [Int], postorder: [Int]) -> BinaryTreeNode? {
+  guard let rootVal = postorder.last else { return nil }
+  let root = BinaryTreeNode(rootVal)
+  
+  let index = inorder.firstIndex(of: rootVal)!
+  let leftInorder = Array(inorder[..<index])
+  let rightInorder = Array(inorder[(index + 1)...])
+  
+  let newPostorder = Array(postorder.dropLast())
+  
+  let rightPostorder: [Int] = Array(newPostorder.suffix(rightInorder.count))
+  let leftPostorder: [Int] = Array(newPostorder.prefix(newPostorder.count - rightInorder.count))
+  
+  root.leftChild = buildTree(inorder: leftInorder, postorder: leftPostorder)
+  root.rightChild = buildTree(inorder: rightInorder, postorder: rightPostorder)
+  
+  return root
+}
+
+let inorder = [4, 2, 5, 7, 1, 3, 8, 6]
+let postorder = [4, 7, 5, 2, 8, 6, 3, 1]
+let tree2 = buildTree(inorder: inorder, postorder: postorder)
+print("层级遍历:")
+levelOrderTraverse(tree2)
+print("先序遍历:")
+nonRecursivePreorderTraversal(tree2)
+print("中序遍历:")
+nonRecursiveInorderTraversal(tree2)
+print("后序遍历")
+nonRecureivePostorderTraversal(tree2)
+
+//: [Next](@next)
--- Swift/Swift.playground/Pages/LinkList.xcplaygroundpage/Contents.swift
+++ Swift/Swift.playground/Pages/LinkList.xcplaygroundpage/Contents.swift
@@ -3,149 +3,6 @@
 
 import Foundation
 
-//链表节点
-class ListNode {
-  var value: Int
-  var next: ListNode?
-  init(_ value: Int) {
-    self.value = value
-  }
-}
-
-//创建链表，返回头节点
-func createList(_ values: [Int]) -> ListNode? {
-  var head: ListNode? = nil
-  var trail: ListNode? = nil
-  values.forEach { (value) in
-    let node = ListNode(value)
-    if head == nil {
-      head = node
-    } else {
-      trail!.next = node
-    }
-    trail = node
-  }
-  return head
-}
-
-//根据下标查询节点
-func getNode(from head: ListNode?, at index: Int) -> ListNode? {
-  var i = 0
-  var node: ListNode? = head
-  while i < index && node?.next != nil {
-    node = node?.next
-    i = i + 1
-  }
-  if i != index {
-    return nil
-  }
-  return node
-}
-
-//插入节点
-func insert(node: ListNode?, to head: ListNode, at index: Int) -> ListNode? {
-  guard let node = node else { return head }
-  guard index > 0 else {
-    node.next = head
-    return node
-  }
-  if let pNode = getNode(from: head, at: index - 1) {
-    node.next = pNode.next
-    pNode.next = node
-  }
-  return head
-}
-
-//删除节点
-func deleteNode(from head: ListNode, at index: Int) -> ListNode? {
-  guard index > 0 else {
-    let next = head.next
-    head.next = nil
-    return next
-  }
-  if let pNode = getNode(from: head, at: index - 1) {
-    let node = pNode.next
-    pNode.next = node?.next
-    node?.next = nil
-  }
-  return head
-}
-
-//修改节点
-func updateNode(_ head: ListNode, at index: Int, use value: Int) -> ListNode {
-  getNode(from: head, at: index)?.value = value
-  return head
-}
-
-func printList(_ head: ListNode?) {
-  var node = head
-  var nodes: [Int] = []
-
-  while node != nil {
-    nodes.append(node!.value)
-    node = node!.next
-  }
-  print(nodes)
-}
-
-/*
- 给一个链表和一个值x，要求将链表中所有小于x的值放到左边，所有大于等于x的值放到右边。原链表的节点顺序不能变。
- 例：1->5->3->2->4->2，给定x = 3。则我们要返回 1->2->2->3->5->4
- */
-func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
-  let prevDummy = ListNode(0)
-  var prev = prevDummy
-  let postDummy = ListNode(0)
-  var post = postDummy
-  
-  let currDummy = ListNode(x)
-  
-  var node = head
-  
-  while node != nil {
-    if node!.value < x {
-      prev.next = node
-      prev = node!
-    } else if node!.value > x {
-      post.next = node
-      post = node!
-    }
-    node = node!.next
-  }
-  
-  post.next = nil
-  prev.next = currDummy
-  currDummy.next =  postDummy.next
-  
-  return prevDummy.next
-}
-
-let listHead = createList([1, 5, 3, 2, 4, 2])
-printList(listHead)
-
-partition(listHead, 3)
-printList(listHead)
-
-/*
- 给定一个从小到大的数组 [2, 3, 5, 8, 9, 10, 18, 26, 32]，要求往数组中插入6，使原数组依然从小到大排序。
- */
-func modify() {
-  let array = [2, 3, 5, 8, 9, 10, 18, 26, 32]
-  let value = 6
-  let head = createList(array)
-  var i = 0
-  var node = head!
-  var v = node.value
-  while node.next != nil && v < value {
-    node = node.next!
-    v = node.value
-    i = i + 1
-  }
-  insert(node: ListNode(6), to: head!, at: i)
-  printList(head)
-}
-modify()
-
 //: ## 使用枚举实现
 
 // 链表
@@ -162,7 +19,9 @@ indirect enum LinkedList<Element: Comparable> {
     }
     return total
   }
+}
 
+extension LinkedList {
   //获取节点
   func node(at index: Int) -> LinkedList {
     guard case let .node(_, next) = self else {
@@ -173,7 +32,7 @@ indirect enum LinkedList<Element: Comparable> {
     }
     return next.node(at: index - 1)
   }
-
+  
   func element(at index: Int) -> Element? {
     let node = self.node(at: index)
     if case let .node(element, _) = node {
@@ -181,7 +40,7 @@ indirect enum LinkedList<Element: Comparable> {
     }
     return nil
   }
-
+  
   //修改节点
   mutating func modify(element: Element, at index: Int) {
     guard case let .node(e, n) = self else {
@@ -195,7 +54,7 @@ indirect enum LinkedList<Element: Comparable> {
       self = LinkedList.node(e, next)
     }
   }
-
+  
   //删除节点
   mutating func delete(element: Element) {
     guard case .node(let value, var next) = self else {
@@ -204,7 +63,7 @@ indirect enum LinkedList<Element: Comparable> {
     next.delete(element: element)
     self = element == value ? next : LinkedList.node(value, next)
   }
-
+  
   mutating func delete(at index: Int) {
     guard case .node(let e, var n) = self else {
       return
@@ -216,11 +75,11 @@ indirect enum LinkedList<Element: Comparable> {
     n.delete(at: index - 1)
     self = LinkedList.node(e, n)
   }
-
+  
   mutating func deleteLast() {
     delete(at: self.length - 1)
   }
-
+  
   //增加节点
   mutating func insert(_ element: Element, at index: Int) {
     guard index > 0 else {
@@ -233,11 +92,11 @@ indirect enum LinkedList<Element: Comparable> {
     n.insert(element, at: index - 1)
     self = LinkedList.node(e, n)
   }
-
+  
   mutating func insert(head: Element) {
     insert(head, at: 0)
   }
-
+  
   mutating func append(_ trail: Element) {
     guard case .node(let value, var next) = self else {
       self = LinkedList.node(trail, .empty)
@@ -246,7 +105,6 @@ indirect enum LinkedList<Element: Comparable> {
     next.append(trail)
     self = LinkedList.node(value, next)
   }
-
 }
 
 extension LinkedList: CustomStringConvertible {

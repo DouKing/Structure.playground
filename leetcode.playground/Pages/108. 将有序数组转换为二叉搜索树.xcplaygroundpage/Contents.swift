489a29a2446bf1aab959b0fb750e3f0b442aa7dd
DouKing <wyk1016@126.com>
2020-08-07 22:58:37 +0800

    将有序数组转换为二叉搜索树

---
+++ leetcode.playground/Pages/108. 将有序数组转换为二叉搜索树.xcplaygroundpage/Contents.swift
@@ -0,0 +1,50 @@
+//: [Previous](@previous)
+/*
+ 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ 
+ 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ 
+ 示例:
+ 
+ 给定有序数组: [-10,-3,0,5,9],
+ 
+ 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+ 
+      0
+     / \
+   -3   9
+  /   /
+ -10  5
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+import Foundation
+
+class Solution {
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        return create(nums, left: 0, right: nums.count - 1)
+    }
+    
+    func create(_ nums: [Int], left: Int, right: Int) -> TreeNode? {
+        if left > right {
+            return nil
+        }
+        let mid = (left + right + 1) >> 1
+        let root = TreeNode(nums[mid])
+        root.left = create(nums, left: left, right: mid - 1)
+        root.right = create(nums, left: mid + 1, right: right)
+        return root
+    }
+}
+
+let solution = Solution()
+guard let root = solution.sortedArrayToBST([-10, -3, 0, 5, 9]) else {
+    fatalError()
+}
+
+print(root)
+
+print("Done!")
+//: [Next](@next)

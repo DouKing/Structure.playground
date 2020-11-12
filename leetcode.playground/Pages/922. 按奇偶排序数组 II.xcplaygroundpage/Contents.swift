7a325a6ffe7ce0057461697cfbdf0ea329a87eb0
DouKing <wyk1016@126.com>
2020-11-12 21:46:29 +0800

    按奇偶排序数组 II

---
+++ leetcode.playground/Pages/922. 按奇偶排序数组 II.xcplaygroundpage/Contents.swift
@@ -0,0 +1,69 @@
+//: [Previous](@previous)
+/**
+ 给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+ 
+ 对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+ 
+ 你可以返回任何满足上述条件的数组作为答案。
+ 
+   
+ 
+ 示例：
+ 
+ 输入：[4,2,5,7]
+ 输出：[4,5,2,7]
+ 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+   
+ 
+ 提示：
+ 
+ 2 <= A.length <= 20000
+ A.length % 2 == 0
+ 0 <= A[i] <= 1000
+ 
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/sort-array-by-parity-ii
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+import Foundation
+
+class Solution {
+    func sortArrayByParityII(_ A: [Int]) -> [Int] {
+        guard !A.isEmpty else {
+            return []
+        }
+        var a: [Int] = []
+        var b: [Int] = []
+        var ans: [Int] = []
+        
+        var i = 0
+        for item in A {
+            if item % 2 == 0 {
+                a.append(item)
+                i += 2
+            }
+        }
+        
+        i = 1
+        for item in A {
+            if item % 2 == 1 {
+                b.append(item)
+                i += 1
+            }
+        }
+        
+        for i in 0 ..< a.count {
+            ans.append(a[i])
+            ans.append(b[i])
+        }
+        
+        return ans
+    }
+}
+
+let solution = Solution()
+solution.sortArrayByParityII([4, 2, 5, 7])
+
+print("Done!")
+//: [Next](@next)
